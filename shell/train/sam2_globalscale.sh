8d5832fe80c5286dd116bc2ba8b2ab70e9d77762
Stanford Ward <46523891+ShqWW@users.noreply.github.com>
2026-08-16 22:05:12 +0800

    update the train shell

--- shell/train/sam2_globalscale.sh
+++ shell/train/sam2_globalscale.sh
@@ -1,11 +1,11 @@
-# #python train.py --gpu_no 4 \
-# CUDA_VISIBLE_DEVICES=4,5,6,7 OMP_NUM_THREADS=1 \
-# torchrun --nproc_per_node=4 --master_port=23458 \
-# train.py --is_multigpu 1 \
-# --cfg ./Config/sam2_globalscale.py \
-# --save_path work_dir/sam2_globalscale_seg \
-# --iter_display 1000 \
-# --train_vector 0
+#python train.py --gpu_no 4 \
+CUDA_VISIBLE_DEVICES=4,5,6,7 OMP_NUM_THREADS=1 \
+torchrun --nproc_per_node=4 --master_port=23458 \
+train.py --is_multigpu 1 \
+--cfg ./Config/sam2_globalscale.py \
+--save_path work_dir/sam2_globalscale_seg \
+--iter_display 1000 \
+--train_vector 0
 
 CUDA_VISIBLE_DEVICES=4,5,6,7 OMP_NUM_THREADS=1 \
 torchrun --nproc_per_node=4 --master_port=23458 \
